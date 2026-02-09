fc3353ebf3599903b17cb929db00501bd2400b0a
QuantumRobot243 <aliarif1168@gmail.com>
2026-02-09 11:29:44 +0530

    tanh to bound the bilion percent infection rate

--- src/training.jl
+++ src/training.jl
@@ -5,6 +5,9 @@ function ude_model!(du, u, p, t, nn, st)    ##nn the lux neural net structure an
 
     nn_guess, _ = nn([I], p.neural, st)    ##hetwork looks at the current number of infected machine which is I and passess this through the weight stored in p.neural
 
+    correction = tanh(nn_val[1])  ## nOw this will prevents the nn from suf=ggesting a billion percent infection rate
+
+
     infection_term = p.physics[1] * S * I + nn_guess[1]    #p.physics[1] * S * I  is infection term suspected node ,infected node ...beta scales contagious the diseas is
 
     du[1] = -infection_term
