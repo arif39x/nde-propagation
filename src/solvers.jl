fd045ba1d7c3240a1db59f3a37115361b04c4b33
QuantumRobot243 <aliarif1168@gmail.com>
2026-02-09 11:02:09 +0530

    Fix:Update abstol and reltol for handel fast changing and tighter tolerances

--- src/solvers.jl
+++ src/solvers.jl
@@ -1,7 +1,7 @@
-using DifferentialEquations  #To solve the calculas it gives the algorythm like Tsit5 that step through time to see hoe malware spreads
+using DifferentialEquations #To solve the calculas it gives the algorythm like Tsit5 that step through time to see hoe malware spreads
 
 function predict(u0, p, tspan, nn, st)    #u0 initial condition, p combined parametes(Phy+neuralnet) tspan the time window
     prob = ODEProblem((du, u, p, t) -> ude_model!(du, u, p, t, nn, st), u0, tspan, p) #The one of the most intersting logic that i wrote ok let me explain
     # it creates a hybrid math model ehre the system changes over time is defined by combining physics laws and the neural network's real time prediction....
-    return solve(prob, Tsit5(), saveat=0.5)     ##e Tsitouras 5/4 Runge-Kutta method...and the saveat = 0.5 solves the record the state every 0.5 times units
+    return solve(prob, AutoTsit5(Rosenbrock23()), saveat=0.5, reltol=1e-6, abstol=1e-6)
 end
