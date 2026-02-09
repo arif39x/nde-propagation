bc6a16ac9ef5366deb46dff54ecdf7ae60d735df
QuantumRobot243 <aliarif1168@gmail.com>
2026-02-09 11:20:38 +0530

    Fix:10f7 -> 1.0f7

--- scripts/train_ude.jl
+++ scripts/train_ude.jl
@@ -7,7 +7,7 @@ include("../src/training.jl")
 
 data = CSV.read("data/malware_data.csv", DataFrame)
 t_steps = data.timestamp
-observed_I = data.infected_noisy
+observed_I = Float32.(data.infected_noisy)
 
 nn = create_nn()
 rng = Random.default_rng()
@@ -24,15 +24,17 @@ p_init = ComponentVector(
 function loss(p, _)
     sol = predict(u0, p, tspan, nn, st)
 
-    if sol.retcode != ReturnCode.Success
-        return 1e7f0  # if the sover falles it return high penalty not inf and keep the gradient defined
+
+    if sol.retcode != ReturnCode.Success    # Return a high penalty if unstable to keep the optimizer away from bad zones
+        return 1.0f7
     end
 
     pred_I = sol[2, :]
 
-    data_loss = sum(abs2, pred_I .- observed_I) #meansquare error standard
 
-    reg_loss = 0.01f0 * sum(abs2, p.neural)
+    data_loss = sum(abs2, pred_I .- observed_I)   # Mean Square Error
+
+    reg_loss = 0.01f0 * sum(abs2, p.neural) #keeps weights small to prevent the ODE from exploding
 
     return data_loss + reg_loss
 end
@@ -41,8 +43,9 @@ adtype = Optimization.AutoZygote()
 optf = OptimizationFunction(loss, adtype)
 optprob = OptimizationProblem(optf, p_init)
 
-println("Stabilizing with Adam arounf 200 iterations...")
+println("Stabilizing with Adam (200 iterations)...")
 res_adam = solve(optprob, OptimizationOptimisers.Adam(0.005f0), maxiters=200)
+
 
 println("Refining with BFGS...")
 optprob_bfgs = remake(optprob, u0=res_adam.u)
@@ -53,9 +56,10 @@ final_sol = predict(u0, p_trained, tspan, nn, st)
 
 p_plot = plot(t_steps, observed_I, seriestype=:scatter, label="Noisy Data (Input)", mc=:black, ms=3, ma=0.4)
 plot!(final_sol.t, final_sol[2, :], label="UDE Learned Curve", lw=3, lc=:red)
-title!("Malware Dynamics")
+title!("Malware Dynamics Discovery")
 xlabel!("Time")
 ylabel!("Infected Ratio")
 
+mkpath("plots")
 savefig(p_plot, "plots/ude_results.png")
-println("Training complete.")
+println("Training complete. ")
